583d9489d4df836e585e0d7ebeb4ee7ce4e0029a
atomofiron <atomofiron@gmail.com>
2019-09-15 13:37:52 +0300

    добавил отображение надписи при нажатии кнопки

--- app/src/main/java/io/atomofiron/myapplication/MainActivity.java
+++ app/src/main/java/io/atomofiron/myapplication/MainActivity.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+    private TextView label;
     private Button button;
 
     @Override
@@ -14,6 +16,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        label = findViewById(R.id.activity_tv_label);
         button = findViewById(R.id.activity_btn_kek);
 
         button.setOnClickListener(new View.OnClickListener() {
@@ -23,5 +26,6 @@ public class MainActivity extends Activity {
 
     private void onButtonKekClick(View v) {
         I.log(this, "kek");
+        label.setText(getString(R.string.label_btn_clicked));
     }
 }
