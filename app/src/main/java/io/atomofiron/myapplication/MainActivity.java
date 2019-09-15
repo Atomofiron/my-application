930218ea495a11df143efde735c80d863f008019
atomofiron <atomofiron@gmail.com>
2019-09-15 12:54:23 +0300

    добавил кнопку

--- app/src/main/java/io/atomofiron/myapplication/MainActivity.java
+++ app/src/main/java/io/atomofiron/myapplication/MainActivity.java
@@ -2,12 +2,25 @@ package io.atomofiron.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.activity_btn_kek);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) { onButtonKekClick(v); }
+        });
+    }
+
+    private void onButtonKekClick(View v) {
     }
 }
